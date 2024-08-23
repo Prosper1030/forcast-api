fbd96d1cfde02660a5dc994421ef7eef99bc8648
Prosper1030 <1030railgunjudgelight@gmail.com>
2024-08-23 19:19:24 +0800

    Add widget

--- main.py
+++ main.py
@@ -1,4 +1,10 @@
 import streamlit as st
 import pandas as pd
 
-st.title("Weather Forcast Cast of the next Days")
+st.header("Weather Forcast Cast for the next Days")
+place = st.text_input("Place: ")
+days = st.slider("forcast Days", min_value=1, max_value=5,
+                 help="Select a number of forecasted days")
+option = st.selectbox("Select data to view",
+                      ("Temperature", "Sky"))
+st.subheader(f"{option} for the next {days} days in {place}")
